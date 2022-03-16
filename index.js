787f67f9811677ffee1afb34140dae922333d604
kayla <kay.ann2000@gmail.com>
2022-03-16 16:47:56 -0600

    commit tree 31ed25f7696111bd245e2b3e20d5b87273ba9ef3

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+// Code your solutions in this file;
+
+function writeCards(names, event){
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+        messages.push("Thank you, " + names[i]+ ", for the wonderful " + event + " gift!");
+        console.log(messages[i])
+      }
+      return messages;
+    }
+function countDown( number) {
+    for( ; number>= 0 ; number--){
+        console.log(number);
+    }
+}
